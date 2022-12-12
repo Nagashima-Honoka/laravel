d07796f3ee520b7b4a734b52af93d2bc7da5f4d2
永島帆香 <nagashimahonoka@nagashimahankounoMacBook-Pro.local>
2022-12-12 10:59:05 +0900

    each

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -6,40 +6,15 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    // form
-    // public function index() {
-    //     $data = [
-    //         'msg'=>'お名前を入力してください。',
-    //     ];
-    //     return view('hello.index', $data);
-    // }
-
-    // public function post(Request $request) {
-    //     $msg = $request->msg; // name="msg"取得
-    //     $data = [
-    //         'msg'=>'こんにちは、' . $msg . 'さん！',
-    //     ];
-    //     return view('hello.index', $data);
-    // }
-
-    // ifディレクティブ
-    //    public function index() {
-    //     return view('hello.index', ['msg'=>'']);
-    //    }
-
-    // issetディレクティブ
-    // public function index() { // GET時には$msgは未定義
-    //     return view('hello.index');
-    // }
-
-    public function post(Request $request) { // POSTされると値が渡される
-    return view('hello.index', ['msg'=>$request->msg]);
-    }
-
-    // foreachディレクティブ
+    // eachによるコレクションビュー
+    // data変数を用意しておく
     public function index() {
-        $data = ['one', 'two', 'three', 'four', 'five'];
-        return view('hello.index', ['data'=>$data]); // $dataを'data'に設定してテンプレートに渡す
+        $data = [
+            ['name'=>'山田たろう', 'mail'=>'taro@yamada'],
+            ['name'=>'田中はなこ', 'mail'=>'hanako@flower'],
+            ['name'=>'鈴木さちこ', 'mail'=>'sachico@happy'],
+        ];
+        return view('hello.index', ['data'=>$data]);
     }
 
 
--- src/resources/views/hello/index.blade.php
+++ src/resources/views/hello/index.blade.php
@@ -12,17 +12,8 @@
 
 @section('content')
  <p>ここが本文のコンテンツです。</p>
- <p>必要なだけ記述できます。</p>
 
-  @component('components.message')
-   @slot('msg_title')
-   CAUTION!
-   @endslot
-
-   @slot('msg_content')
-   これはメッセージの表示です。
-   @endslot
-  @endcomponent
+  @each('components.item', $data, 'item' )
 @endsection
 
 @section('footer')
