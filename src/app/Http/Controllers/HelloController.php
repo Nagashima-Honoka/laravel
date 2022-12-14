8c62ba3b2cd846df3a5d77fe69839161059787d1
永島帆香 <nagashimahonoka@nagashimahankounoMacBook-Pro.local>
2022-12-14 14:20:33 +0900

    ミドルウェアを実行

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -6,11 +6,8 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    // eachによるコレクションビュー
-    // data変数を用意しておく
-    public function index() {
-     return view('hello.index', ['message'=>'Hello!']);
+    public function index(Request $request) {
+        return view('hello.index', ['data'=>$request->data]);
     }
-
 
 }
