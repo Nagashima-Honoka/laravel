336516a11b5e38af0778ec644a31cd6b9670d88d
永島帆香 <nagashimahonoka@nagashimahankounoMacBook-Pro.local>
2022-12-14 23:43:57 +0900

    エラーメッセージと値の保持

--- src/resources/views/hello/index.blade.php
+++ src/resources/views/hello/index.blade.php
@@ -9,25 +9,37 @@
 
 @section('content')
  <p>{{ $msg }}</p>
+ <!-- $errorsという変数は、バリデーションで発生したエラーメッセージをまとめて管理するオブジェクト。バリデーションの機能によって組み込まれる。 -->
+ @if(count($errors) > 0)
+ <div>
+    <ul>
+<!-- $errors->all()はエラーメッセージを配列にまとめて取り出す -->
+        @foreach($errors->all() as $error)
+         <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+ </div>
+ @endif
  <table>
     <form action="/hello" method="POST">
      {{ csrf_field() }}
      <tr>
         <th>name:</th>
         <td>
-            <input type="text" name="name">
+            <!-- old('値'): 引数に指定した入力項目の古い値（現在の値が設定される前の値）を返す -->
+            <input type="text" name="name" value="{{ old('name') }}">
         </td>
      </tr>
      <tr>
         <th>mail:</th>
         <td>
-            <input type="text" name="mail">
+            <input type="text" name="mail" value="{{ old('mail') }}">
         </td>
      </tr>
      <tr>
         <th>age:</th>
         <td>
-            <input type="text" name="age">
+            <input type="text" name="age" value="{{ old('age') }}">
         </td>
      </tr>
      <tr>
