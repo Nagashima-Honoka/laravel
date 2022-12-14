3f7ee68f884fb8ca4ed3f29655e4ed4fddfc899a
永島帆香 <nagashimahonoka@nagashimahankounoMacBook-Pro.local>
2022-12-14 23:08:54 +0900

    バリデーションの基本処理

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -7,7 +7,17 @@ use Illuminate\Http\Request;
 class HelloController extends Controller
 {
     public function index(Request $request) {
-        return view('hello.index');
+        return view('hello.index', ['msg'=>'フォームを入力:']); // msgという値を用意してviewを呼び出すだけ
+    }
+
+    public function post(Request $request) {
+        $validate_rule = [
+            'name' => 'required',
+            'mail'=> 'email',
+            'age'=>'numeric|between:0,150', // 値が数値である | 0~150である
+        ];
+        $this->validate($request, $validate_rule); // validationの実行
+        return view('hello.index', ['msg'=>'正しく入力されました！']);
     }
 
 }
