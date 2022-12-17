adc62aa9fc2b4b1c9e6283b5c1c1fe6d9557c21c
永島帆香 <nagashimahonoka@nagashimahankounoMacBook-Pro.local>
2022-12-17 14:19:28 +0900

    オリジナル・バリデータの作成

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -3,52 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\HelloRequest;
 
 class HelloController extends Controller
 {
     public function index(Request $request) {
-        $validator = Validator::make($request->query(), [
-            'id' => 'required',
-            'pass' => 'required',
-        ]);
-        if($validator->fails()) {
-            $msg = 'クエリーに問題があります。';
-        } else {
-            $msg = 'ID/PASSを受け付けました。フォームを入力ください。';
-         }
-        return view('hello.index', ['msg'=>'$msg, ']);
+        return view('hello.index', ['msg'=>'フォームを入力ください。 ']);
     }
 
-    public function post(Request $request) {
-        $rules = [
-            'name' => 'required',
-            'mail' => 'email',
-            'age' => 'numeric|between:0,150',
-        ];
-
-        $messages = [
-            'name.required' => '名前は必ず入力してください。', // '項目名.ルール名' => 'メッセージ'
-            'mail.email' => 'メールアドレスが必要です。',
-            'age.numeric' => '年齢を整数で記入ください。',
-            'age.between' => '年齢は0~150の間で入力ください。',
-            'age.min' => '年齢はゼロ歳以上で記入ください。',
-            'age.max' => '年齢は200歳以下で記入ください。',
-        ];
-
-        $validator = Validator::make($request->all(), $rules, $messages);
-
-        $validator->sometimes('age', 'min:0', function($input) { // 第3引数のクロージャで、!is_int($input->age)の値を返す
-            return !is_int($input->age); // これにより、'min:0'のルールが'age'に追加される
-        });
-        $validator->sometimes('age', 'max:200', function($input) {
-            return !is_int($input->age);
-        });
-
-        if($validator->fails()) {
-            return redirect(('/hello'))->withErrors($validator)->withInput();
-        }
+    public function post(HelloRequest $request) {
         return view('hello.index', ['msg'=>'正しく入力されました！']);
     }
-
 }
--- src/app/Providers/HelloServiceProvider.php
+++ src/app/Providers/HelloServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Validators\HelloValidator;
 
 class HelloServiceProvider extends ServiceProvider
 {
@@ -24,8 +26,9 @@ class HelloServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        View::composer(
-            'hello.index', 'App\Http\Composers\HelloComposer' // 第2引数に呼び出すクラス名をテキスト値で指定する
-        );
+        $validator = $this->app['validator'];
+        $validator-> resolver(function($translator, $data, $rules, $messages) {
+            return new HelloValidator($translator, $data, $rules, $messages);
+        });
     }
 }
