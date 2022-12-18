0323803b967c80a6d80cf7584876f8912f9f1bff
永島帆香 <nagashimahonoka@nagashimahankounoMacBook-Pro.local>
2022-12-18 15:49:51 +0900

    パラメータ結合の利用

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -9,7 +9,12 @@ use Illuminate\Support\Facades\DB;
 class HelloController extends Controller
 {
     public function index(Request $request) {
-        $items = DB::select('select * from people');
+        if(isset($request->id)) {
+            $param = ['id' => $request->id]; // ['id' => パラメータ]
+            $items = DB::select('select * from people where id = :id', $param); // :id = パラメータの値をはめ込むプレースホルダ（値を確保しておく場所のこと）
+        } else {
+            $items = DB::select('select * from people');
+        }
         return view('hello.index', ['items'=> $items]);
     }
 
