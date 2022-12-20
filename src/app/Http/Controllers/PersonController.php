626911b51adfa0cfb28aecf939bbf7286422e963
永島帆香 <nagashimahonoka@nagashimahankounoMacBook-Pro.local>
2022-12-20 16:36:44 +0900

    関連レコードの有無

--- src/app/Http/Controllers/PersonController.php
+++ src/app/Http/Controllers/PersonController.php
@@ -9,8 +9,10 @@ use Illuminate\Support\Facades\Redis;
 class PersonController extends Controller
 {
     public function index(Request $request){
-        $items = Person::all(); // 全レコードを取得。取得されたレコードはIlluminate\Database\Eloquent名前空間のCollectionクラスのインスタンスとして得られる。Collectionクラスはレコード管理専用のコレクションクラス。
-        return view('person.index', ['items' => $items]);
+        $hasItem = Person::has('boards')->get();
+        $noItem = Person::doesntHave('boards')->get();
+        $param = ['hasItem' => $hasItem, 'noItem' => $noItem];
+        return view('person.index', $param);
     }
 
     public function find(Request $request) { // /findにGETアクセスした時の処理
--- src/resources/views/person/index.blade.php
+++ src/resources/views/person/index.blade.php
@@ -13,22 +13,29 @@
         <th>Person</th>
         <th>Board</th>
     </tr>
-    @foreach($items as $item)
+    @foreach($hasItem as $item)
     <tr>
         <td>{{ $item->getData() }}</td>
         <td>
-            @if($item->boards != null)
-            <!-- $item->boardsがnullでなければ繰り返し処理を行う -->
             <table width="100%">
-            <!-- $item->boardsから順に値を$objに取り出し、そこからgetData()の内容を出力する。こうすることで、boardsに関連付けられた全てのレコードの値を表示する -->
                 @foreach($item->boards as $obj)
                  <tr>
                     <td>{{ $obj->getData() }}</td>
                  </tr>
                 @endforeach
             </table>
-            @endif
         </td>
+    </tr>
+    @endforeach
+ </table>
+ <div style="margin: 10px;"></div>
+ <table>
+    <tr>
+        <th>Person</th>
+    </tr>
+    @foreach($noItem as $item)
+    <tr>
+        <td>{{ $item->getData() }}</td>
     </tr>
     @endforeach
  </table>
