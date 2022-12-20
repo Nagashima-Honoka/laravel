ee238c2d22fc13b1b34495243fbf86261ef0c520
永島帆香 <nagashimahonoka@nagashimahankounoMacBook-Pro.local>
2022-12-20 10:54:31 +0900

    モデルの新規保存

--- src/app/Http/Controllers/PersonController.php
+++ src/app/Http/Controllers/PersonController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Person;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class PersonController extends Controller
 {
@@ -22,6 +23,26 @@ class PersonController extends Controller
         $item = Person::ageGreaterThan($min)->ageLessThan($max)->first(); // ageGreaterThan = ○○以上, ageLessThan = ○○以下
         $param = ['input' => $request->input, 'item' => $item];
         return view('person.find', $param);
+    }
+
+    public function add(Request $request) {
+        return view('person.add');
+    }
+
+    public function create(Request $request) {
+        $this->validate($request, Person::$rules); // 1. バリデーションの実行
+        $person = new Person; // 2. Personインスタンスの作成。バリデーションを通過したらモデルの保存作業を行う。
+        $form = $request->all(); // 3. 値を用意する
+        unset($form['_token']); // フォームに追加される表示フィールドの_tokenだけはunsetで削除しておく。_tokenという値はCSRF用非表示フィールドとして用意される値。これ自身は、テーブルにはないフィールドのため、あらかじめ削除いておく。
+        $person->timestamps = false; // 「updated_atカラム」が無いとエラーが出たためタイムスタンプを無効にする
+        $person->fill($form)->save(); // 4. インスタンスに値を設定して保存。fill()は、引数に用意されている配列の値をモデルのプロパティに代入するもの。フォームのようにまとまった値がある場合は、fill()を使うことで、個々のプロパティをまとめて設定できる。値が設定されたら、save()を呼び出してインスタンスを保存する。
+        // 1つ1つの値をインスタンスに設定していっても同じ
+        // $person = new Person;
+        // $person->name = $request->name;
+        // $person->mail = $request->mail;
+        // $person->age = $request->age;
+        // $person->save();
+        return redirect('/person');
     }
 
 }
