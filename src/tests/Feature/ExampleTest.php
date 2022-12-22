56228a69e1852d597dc7664897805f7df17f1807
永島帆香 <nagashimahonoka@nagashimahankounoMacBook-Pro.local>
2022-12-22 17:11:59 +0900

    ExampleTest.phpの内容確認

--- src/tests/Feature/ExampleTest.php
+++ src/tests/Feature/ExampleTest.php
@@ -6,13 +6,13 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
-{
+{ // テスト処理
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function test_example()
+    public function test_example() // testで始まるメソッド名はテスト用のメソッドと判断される
     {
         $response = $this->get('/');
 
